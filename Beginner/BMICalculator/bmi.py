66319a592170f1a5bdef5acbeaaa64fe75126072
elichka <elichka.cs@outlook.com>
2023-10-07 15:42:33 +0300

    Add tip.py and weeks.py

--- Beginner/BMICalculator/bmi.py
+++ Beginner/BMICalculator/bmi.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
     weight = float(input("Enter weight in kg: "))
     height = float(input("Enter height in m: "))
     bmi = weight / height ** 2
-    print(int(bmi))
+    print(bmi)
--- Beginner/TipCalculator/tip.py
+++ Beginner/TipCalculator/tip.py
@@ -1,3 +1,7 @@
 if __name__ == '__main__':
-    num = input()
-    print(int(num[0]) + int(num[1]))
+    print("Welcome to the tip calculator.")
+    bill = float(input("What was the total bill? $"))
+    tip = int(input("What percentage tip would you like to give? "))
+    people = int(input("How many people to split the bill? "))
+    bill_per_person = (bill + 0.01*tip*bill)/people
+    print(f"Each person should pay: ${round(bill_per_person, 2)}")
